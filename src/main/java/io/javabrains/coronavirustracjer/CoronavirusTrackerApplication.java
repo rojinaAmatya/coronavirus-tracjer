cc7a3feb9ecf53ac8e6943d7ef59b7a4b9d53e0f
rojinaAmatya <rojinaamatya1432@gmail.com>
2022-01-12 14:59:35 -0600

    added csv to print only the states

--- src/main/java/io/javabrains/coronavirustracjer/CoronavirusTrackerApplication.java
+++ src/main/java/io/javabrains/coronavirustracjer/CoronavirusTrackerApplication.java
@@ -2,8 +2,10 @@ package io.javabrains.coronavirustracjer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling //spring knows needs to run the schedule
 public class CoronavirusTrackerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/io/javabrains/coronavirustracjer/services/CoronaVirusDataService.java
+++ src/main/java/io/javabrains/coronavirustracjer/services/CoronaVirusDataService.java
@@ -1,26 +1,43 @@
 package io.javabrains.coronavirustracjer.services;
 
+import io.javabrains.coronavirustracjer.modules.LocationStats;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service //makes a spring service and executes
 public class CoronaVirusDataService {
 
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
+    private List<LocationStats> allStats = new ArrayList<>();
     @PostConstruct
+    @Scheduled(cron = "* * 1 * * *") //offers to run method on a regular basis
     public void fetchVirusData() throws IOException, InterruptedException {
+        List<LocationStats> newStats = new ArrayList<>();
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request= HttpRequest.newBuilder().uri(URI.create(VIRUS_DATA_URL)).build();
 
         HttpResponse<String> httpResponse= client.send(request, HttpResponse.BodyHandlers.ofString());
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
 
-        System.out.println(httpResponse.body());
+        for (CSVRecord record : records) {
+            String state = record.get("Province/State");
+            System.out.println(state);
+        }
     }
 }
