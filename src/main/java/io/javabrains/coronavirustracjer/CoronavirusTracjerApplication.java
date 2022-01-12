0d2d935cd9a8f46c4949422f216a438c6c673dd6
Rojina <rojina@macs-iMac.home>
2022-01-12 14:00:10 -0600

    Created base project for CoronaVirus tracker via Sprintboot and maven

---
+++ src/main/java/io/javabrains/coronavirustracjer/CoronavirusTracjerApplication.java
@@ -0,0 +1,13 @@
+package io.javabrains.coronavirustracjer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavirusTracjerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusTracjerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/javabrains/coronavirustracjer/CoronavirusTracjerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.coronavirustracjer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTracjerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
